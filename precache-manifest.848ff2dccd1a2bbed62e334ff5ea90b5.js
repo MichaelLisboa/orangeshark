ce9eadb81f2a9a58026fcf61054159c4fc1b0961
Michael <michael@pixt.us>
2019-12-19 21:25:29 +0300

    Updates

---
+++ precache-manifest.848ff2dccd1a2bbed62e334ff5ea90b5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9e7db484dd23d88b80f3c0d40f4da4c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d75e5be0622e52ba5b51",
+    "url": "./static/css/2.af27e82a.chunk.css"
+  },
+  {
+    "revision": "63a8a664f2b7ff46dd8d",
+    "url": "./static/css/main.fa13e475.chunk.css"
+  },
+  {
+    "revision": "d75e5be0622e52ba5b51",
+    "url": "./static/js/2.d0d3be27.chunk.js"
+  },
+  {
+    "revision": "40aad3a8e714aaa8b071e4e33d516e64",
+    "url": "./static/js/2.d0d3be27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63a8a664f2b7ff46dd8d",
+    "url": "./static/js/main.419aab97.chunk.js"
+  },
+  {
+    "revision": "832c46d394b780bcf9e9",
+    "url": "./static/js/runtime-main.f3c2cd1c.js"
+  },
+  {
+    "revision": "d5add290753342e71e60580c5f14b0f2",
+    "url": "./static/media/logo-strap.d5add290.png"
+  },
+  {
+    "revision": "6ddc3dcb33c1149476500ea8b739ed35",
+    "url": "./static/media/logo.6ddc3dcb.png"
+  },
+  {
+    "revision": "a25f5cb3d4e5944216b753cb3700462c",
+    "url": "./static/media/michael-d.a25f5cb3.png"
+  }
+]);
