0c5608bd51aacd6f432eda2a2ff5b2d464bfd69b
Michael <michael@pixt.us>
2020-02-07 17:24:23 +0700

    added dashboard view

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -24,7 +24,7 @@ const Login = props =>  {
     useEffect(
         () => {
             if(!token) return;
-            history.push("/campaigns");
+            history.push("/dashboard");
             return () => console.log("CLEANUP")
         }, [token]
     )
